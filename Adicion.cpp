9d5d212001da46f46ac291207da8fc3523c98bb3
leonelpallares <leonelpallares@live.com>
2017-04-02 18:45:06 -0300

    Add files via upload

---
+++ Adicion.cpp
@@ -0,0 +1,17 @@
+/* Adicion
+* LPallares
+* 02/04/2017
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+      int a,b,suma;
+      cout<<"Ingresa numero"<<endl;
+      cin>>a;
+      cout<<"Ingresa numero"<<endl;
+      cin>>b;
+      suma=a+b;
+      cout<<suma;
+   return 0;
+}
